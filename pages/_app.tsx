47d8af8aec5b9f204fffb2d9fda371e28129f1f2
James Harrison <somtochukwujames2000@gmail.com>
2022-12-22 03:30:10 +0100

    chores: finally fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,46 +13,45 @@ import Loader from "../components/Loader";
 import ModalLayout from "../layouts/ModalLayout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const [loader, setLoader] = useState(true);
-		const [timer, setTimer] = useState(3);
+	const [loader, setLoader] = useState(true);
+	const [timer, setTimer] = useState(3);
 
-		// useLocoScroll(!loader);
+	// useLocoScroll(!loader);
 
-		const id = useRef<any>(null);
+	const id = useRef<any>(null);
 
-		const clear = () => {
-			clearInterval(id.current);
-		};
+	const clear = () => {
+		clearInterval(id.current);
+	};
 
-		useEffect(() => {
-			id.current = window.setInterval(() => {
-				setTimer((timer) => timer - 1);
-			}, 1000);
-			return () => clear();
-		}, []);
+	useEffect(() => {
+		id.current = window.setInterval(() => {
+			setTimer((timer) => timer - 1);
+		}, 1000);
+		return () => clear();
+	}, []);
 
-		useEffect(() => {
-			if (timer === 0) {
-				setLoader(false);
-				clear();
-			}
-		}, [timer]);
+	useEffect(() => {
+		if (timer === 0) {
+			setLoader(false);
+			clear();
+		}
+	}, [timer]);
 
-		useEffect(() => {
-			window.dispatchEvent(new Event("resize"));
-		}, [Component]);
-
+	useEffect(() => {
+		window.dispatchEvent(new Event("resize"));
+	}, [Component]);
 
 	return (
 		<div data-scroll-container>
 			{/* style={{ perspective: "1px" }} */}
 			{/* minHeight: "100vh" */}
 			<Provider store={store}>
+				<ToastContainer />
 				{loader ? (
 					<Loader />
 				) : (
 					<ModalLayout>
-						<ToastContainer />
 						<Component {...pageProps} />
 					</ModalLayout>
 				)}
